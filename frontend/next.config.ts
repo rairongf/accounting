16ac8dc1413d3eda0ed0d6ec970fb3d471a2aeef
rairongf <rairon.gf@gmail.com>
2024-11-19 16:32:45 -0300

    chore: change permanent uri redirect

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/',
-        destination: '/dashboard',
+        destination: '/dashboard/summary',
         permanent: true,
       },
     ]
